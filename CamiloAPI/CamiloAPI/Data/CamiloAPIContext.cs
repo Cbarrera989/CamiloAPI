59eaafb17cd16af5e2ec9480cd83595376f938c2
Cbarrera989 <camilo.barrera989@gmail.com>
2023-07-24 16:58:06 -0500

    crea api
    crea api

---
+++ CamiloAPI/CamiloAPI/Data/CamiloAPIContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CamiloAPI.Data.Models;
+
+namespace CamiloAPI.Data
+{
+    public class CamiloAPIContext : DbContext
+    {
+        public CamiloAPIContext (DbContextOptions<CamiloAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CamiloAPI.Data.Models.Client> Clients { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>().ToTable(nameof(Client));
+            //modelBuilder.Entity<UserRole>().ToTable(nameof(UserRole));
+            //modelBuilder.Entity<User>().ToTable(nameof(User));
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ CamiloAPI/CamiloAPI/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using CamiloAPI.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<CamiloAPIContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CamiloAPIContext") ?? throw new InvalidOperationException("Connection string 'CamiloAPIContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<AlimentarBD>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var seedDb = services.GetRequiredService<AlimentarBD>();
+    await seedDb.SeedAsync();
+}
+
